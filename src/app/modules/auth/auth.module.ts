ac4023f99f4c30622ab2626b12f9cf2a00ebfe8e
sjosuerojas <sjosuerojasc@gmail.com>
2023-09-25 09:23:25 -0500

    feat: auth module template

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { UiModule } from '../ui/ui.module';
+import { AuthRoutingModule } from './routes/auth-routing.module';
+
 import { LayoutAuthComponent } from './components/layout-auth.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { AuthRoutingModule } from './routes/auth-routing.module';
 
 @NgModule({
   declarations: [LayoutAuthComponent, SignInComponent, SignUpComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, UiModule],
 })
 export class AuthModule {}
